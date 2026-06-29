259c9f3be8125891409c9c14f2284f88b030db38
khan <acf21s@gmail.com>
2026-06-29 17:05:18 +0900

    구조 변경

--- mainapp/kshell_mainapp.py
+++ mainapp/kshell_mainapp.py
@@ -1,26 +1,18 @@
 
 import os
 
-#외부 라이브러리
 from lib_include import *
 
 from common_modules.type_hint import *
 
-#main app 관리 helper
+from common_modules.global_resource.kshell_global_resource_manager import KShellGlobalResourceManager
+
 from mainapp.apphelper.kshell_app_helper import KShellAppHelper
 
-#전역 데이터 관리
-from mainapp.global_resource.kshell_global_resource_manager import KShellGlobalResourceManager
-
-#명령 실행 helper
 from mainapp.apphelper.kshell_command_manager import KShellCommandManager
 
-'''
-kshell main app
-'''
 
 class KShellMainApp:
-    
     
     def __init__(self):
         
@@ -54,12 +46,10 @@ class KShellMainApp:
         return ERR_OK
         
     def RunCLICommand(self, dictOpt:dict):
-        
                 
         return self.__commandManager.RunCLICommand(self, dictOpt)
         
     def RunMultiCommand(self, lstMultiCommand:list, dictOpt:dict):
-        
         
         return self.__commandManager.RunMultiCommand(self, lstMultiCommand, dictOpt)
                 
