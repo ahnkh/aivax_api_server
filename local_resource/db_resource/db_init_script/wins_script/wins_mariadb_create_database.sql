29b49083b33bd0772c6581c87348b0943d00d994
khan <acf21s@gmail.com>
2026-09-18 08:09:54 +0900

    toolkit 소스 업데이트 2026-09-18 08:09:54

--- local_resource/db_resource/db_init_script/wins_script/wins_mariadb_create_database.sql
+++ local_resource/db_resource/db_init_script/wins_script/wins_mariadb_create_database.sql
@@ -15,4 +15,15 @@ CREATE TABLE IF NOT EXISTS app.system_integrity_files  (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS smtp_mail_client_setting  (
+
+	`updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `mail_url` varchar(128) NOT NULL,
+  `mail_port` int NOT NULL,
+  `from_mail` varchar(128) NOT NULL ,
+  `use_auth` TINYINT(1) DEFAULT 1,
+  `login_id` varchar(128) DEFAULT '',
+	`login_password` varchar(128) DEFAULT '',
+  `use_tls` TINYINT(1) DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
