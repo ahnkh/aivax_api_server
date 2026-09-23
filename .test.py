e1297208aee79e865d02776cf47a821d66e42262
khan <acf21s@gmail.com>
2026-09-23 11:19:23 +0900

    toolkit 소스 업데이트 2026-09-23 11:19:23

--- .test.py
+++ .test.py
@@ -4,6 +4,12 @@ from lib_include import *
 
 from mainapp.kshell_mainapp import KShellMainApp
 
+def debug_print(kshellMainApp:KShellMainApp):
+    
+    LOG().setLevel(logging.DEBUG)
+    AddStreamLogger()
+    # pass
+
 def init_command(kshellMainApp:KShellMainApp):
         
     kshellMainApp.RunCLICommand({                
@@ -14,29 +20,27 @@ def init_command(kshellMainApp:KShellMainApp):
         "detail_cmd" : "attach_file_backup"    
     })  
     
-    kshellMainApp.RunCLICommand({
-        "method" : ["manage_wins_modules"],
-        "ext_module" : "manage_aivax_pipeline",
-        "cmd_category" : "aivax_pipeline_util",
-        "command" : "aivax_backup",
-        "detail_cmd" : "aivax_disk_rotation"            
-    }) 
+    # kshellMainApp.RunCLICommand({
+    #     "method" : ["manage_wins_modules"],
+    #     "ext_module" : "manage_aivax_pipeline",
+    #     "cmd_category" : "aivax_pipeline_util",
+    #     "command" : "aivax_backup",
+    #     "detail_cmd" : "aivax_disk_rotation"            
+    # }) 
     
-    kshellMainApp.RunCLICommand({
-        "method" : ["manage_wins_modules"],
-        "ext_module" : "manage_aivax_pipeline",
-        "cmd_category" : "aivax_pipeline_util",
-        "command" : "aivax_backup",
-        "detail_cmd" : "aivax_disk_rotation"            
-    })   
-        
+    # kshellMainApp.RunCLICommand({
+    #     "method" : ["manage_wins_modules"],
+    #     "ext_module" : "manage_aivax_pipeline",
+    #     "cmd_category" : "aivax_pipeline_util",
+    #     "command" : "aivax_backup",
+    #     "detail_cmd" : "aivax_disk_rotation"            
+    # })           
     # pass
 
 
 def main():
     
     InitLogger("tracelog.txt", TRACE_LOG_PATH)
-    LOG().setLevel(logging.DEBUG)
     
     kshellMainApp = KShellMainApp()
     
@@ -46,18 +50,15 @@ def main():
     }
     
     kshellMainApp.Initialize(dictOpt)    
-    # pass
     
     dictFunction:dict[str:Any] = {
-        "init" : init_command
+        "debug" : debug_print,
+        "init" : init_command,        
     }
-    
-    command:list[str] = sys.argv[1:]   
     
     for strCommand in sys.argv[1:]:
         
-        command:Any = dictFunction.get(strCommand)     
-        command(kshellMainApp)
+        dictFunction[strCommand](kshellMainApp)
         #break
         
     #pass
